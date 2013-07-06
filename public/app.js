8aecd06beb09cbd743351b1df7e174d6dfe41f17
Vlad Velici <vlad.velici@gmail.com>
2013-07-06 14:50:53 +0300

    initial commit

---
+++ public/app.js
@@ -0,0 +1,111 @@
+var socket, fly, session;
+var displays = [];
+
+var displays = {
+	arr: [],
+	maxDomHeight: 130-4,
+	maxHeight: 0,
+	dom: document.getElementById('screens'),
+	initialScreenData: false,
+
+	add: function (w, h, socketId, order) {
+		var el = document.createElement("div");
+		el.id = 'display_' + socketId;
+		el.width = w;
+		el.height = h;
+		this.dom.appendChild(el);
+
+		this.maxHeight = Math.max(h, this.maxHeight);
+		if (order === undefined) {
+			order = this.arr.length;
+		}
+		this.arr.push({width: w, height: h, id: socketId, dom: el, order: order});
+		// console.log(this.arr);
+		this.updateDom();
+	},
+
+	update: function(id, w, h) {
+		for (var i = 0;i<this.arr.length;i++) {
+			if (this.arr[i].id === id) {
+				this.arr[i].width = w;
+				this.arr[i].height = h;
+				this.updateDom();
+				return;
+			}
+		}
+		this.add(w,h,id);
+	},
+
+	domRatio: function(realW, realH) {
+		var ratio = realW / realH;
+		var h = (this.maxDomHeight * realH) / this.maxHeight;
+		var w = h * ratio;
+		return {width: w, height: h};
+	},
+
+	updateDom: function() {
+		console.log("update dom:", this.arr);
+		for (var i = 0; i<this.arr.length; i++) {
+			var disp = this.arr[i];
+			var res = this.domRatio(disp.width, disp.height);
+			disp.dom.style.width = res.width + "px";
+			disp.dom.style.height = res.height + "px";
+		}
+	},
+
+	rm: function(id) {
+		var index;
+		for (var i = 0;i<this.arr.length;i++) {
+			if (this.arr[i].id === id) {
+				index = i;
+				break;
+			}
+		}
+		if (index === undefined) return;
+		var el = this.arr.splice(index,1)[0];
+		// console.log("the element", el);
+		this.dom.removeChild(el.dom);
+		this.updateDom();
+	}
+}
+
+var init = function() {
+
+	session = socket.socket.sessionid;
+
+	socket.emit("myResolution", {width: window.innerWidth, height: window.innerHeight});
+
+	socket.on("clients", function(data) {
+		displays.initialScreenData = true;
+		// console.log(data);
+		for (var id in data) {
+			displays.add(data[id].width, data[id].height, id, data[id].order);
+		}
+	});
+
+	socket.on("screenData", function(data) {
+		if (displays.initialScreenData)
+			displays.update(data.id, data.width, data.height);
+	});
+
+	socket.on("disconnect", function() {
+		console.log("disconnected");
+	});
+
+	socket.on("clientDisconnected", function(id) {
+		displays.rm(id);
+	});
+}
+
+window.addEventListener("load", function() {
+	socket = io.connect("http://localhost:9000");
+
+	socket.on("connect", init);
+});
+
+window.addEventListener("resize", function() {
+		console.log("resized");
+		if (displays.initialScreenData)
+			displays.update(session, window.innerWidth, window.innerHeight);
+		socket.emit("myResolution", {width: window.innerWidth, height: window.innerHeight});
+})
---
+++ server.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+
+var staticFilesPort = process.env.PORT || 8000;
+var socketioPort = 9000;
+
+var io = require('socket.io').listen(socketioPort);
+
+var clients = {};
+var clientOrder = [];
+var getOrder = function(id) {
+	for (var i=0;i<clientOrder.length; clientOrder++) {
+		if (clientOrder[i] === id)
+			return i;
+	}
+}
+
+
+io.sockets.on('connection', function (socket) {
+
+	var order = clientOrder.length;
+
+	clients[socket.id] = {width:undefined, height:undefined, order: order};
+
+	socket.on("myResolution", function(data) {
+		var sendAfter = false;
+		if (clients[socket.id].width === undefined || clients[socket.id].height === undefined) {
+			sendAfter = true;
+		}
+		clients[socket.id].width = data.width;
+		clients[socket.id].height = data.height;
+
+		if (sendAfter)
+			socket.emit("clients", clients);
+
+		socket.broadcast.emit("screenData", {id: socket.id, width: data.width, height:data.height});
+
+	})
+
+	socket.on("disconnect", function() { 
+		delete clients[socket.id];
+		clientOrder.splice(socket.id,1);
+
+		// update orders
+		for (var sid in clients) {
+			clients[sid].order = getOrder(sid);
+		}
+
+		socket.broadcast.emit("clientDisconnected", socket.id);
+	});
+});
+
+// Static file server:
+var connect = require('connect'),
+    http = require('http');
+
+connect()
+	.use(connect.static('public'))
+	.listen(staticFilesPort);
+
+// Nice greeting so you know it works:
+console.log("Server started at http://localhost:" + staticFilesPort);
+console.log("Socket.io is on port " + socketioPort);
